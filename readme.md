a1da85d952eb6cb753980abedd5b651ab077d4d9
Marcelo Apovian Kayata <marcelo_kayata@hotmail.com>
2024-12-06 00:23:33 -0300

    adicionar na tela

--- readme.md
+++ readme.md
@@ -4,3 +4,12 @@ marcelo@teste.com
 https://github.com/bertdelaspeed/LetsChat_Udemy/blob/main/App.js
 
 montar empresa
+
+
+usuário tutorial:
+1) marcelo@teste.com
+a senha pode ser que seja: 123456
+2) marcelo2@teste.com
+ senha: M@rcelo1994
+3) marcelo3@teste.com
+ senha: M@rcelo1994
--- src/Screens/SportFriend.js
+++ src/Screens/SportFriend.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { collection, getDocs, query, where,  doc,  updateDoc, addDoc } from 'firebase/firestore';
 import { userRef, pageUsersRef, db } from '../../firebase/config';
@@ -6,38 +6,52 @@ import { userRef, pageUsersRef, db } from '../../firebase/config';
 
 const SportFriend = () => {
   const [found, setFound] = useState(false)
-  const [searchFriendsName, setSearchFriendsName] = useState([])
-
+  const [friends, setFriends] = useState([])
+  const renderFriendsNames = ({ item }) => (
+    <Text>{item}</Text>
+  );
   useEffect(()=>  {
     async function SportFriendData(){
+      // usuários
       const queryResult = query(userRef, where('username', '>=', "marc"), where('username', '<=', "marc"+'\uf8ff'))
+      const querySnapshot = await getDocs(queryResult)
+
+      // Esportes
       const querypageUsersRef = query(pageUsersRef)
       // console.log("Dados aqui: ", querypageUsersRef)
       const querypageUsersRefDataSnapshot = await getDocs(querypageUsersRef)
       console.log("dados query: ", querypageUsersRefDataSnapshot)
-      const querySnapshot = await getDocs(queryResult)
       if(!querySnapshot.empty){
         console.log("Entrou aqui")
-        let friends = []
-        querypageUsersRefDataSnapshot.forEach((document)=>{
-          console.log(`dados: `, document)
-          console.log(`document.data(): `, document.data())
-          console.log(`document.id: `, document.id)
-          updateDoc(doc(db, 'PageUsers', document.id), {
-              esportes:
-              {
-                  futebol:true,
-                  futevolei: false,
-                  academia: true,
-                  crossfit: true
-              }
-          }).then(()=>{
-            console.log("Document updated successfully!");
-          })
-          .catch((error) => {
-            console.error("Error updating document: ", error);
-          });
+        // let friends = []
+
+        querySnapshot.forEach(  async (document)=> {
+          console.log(`dados usuarios: `, document.data().username)
+          await setFriends(friends =>[...friends, document.data().username])
+          await console.log("todos os amigos : ", friends)
         })
+        
+
+        // Esportes
+        // querypageUsersRefDataSnapshot.forEach((document)=>{
+        //   console.log(`dados: `, document)
+        //   console.log(`document.data(): `, document.data())
+        //   console.log(`document.id: `, document.id)
+        //   updateDoc(doc(db, 'PageUsers', document.id), {
+        //       esportes:
+        //       {
+        //           futebol:true,
+        //           futevolei: false,
+        //           academia: true,
+        //           crossfit: true
+        //       }
+        //   }).then(()=>{
+        //     console.log("Document updated successfully!");
+        //   })
+        //   .catch((error) => {
+        //     console.error("Error updating document: ", error);
+        //   });
+        // })
         
           // await addDoc(collection(db, "PageUsers"), {
           //   esportes:
@@ -58,6 +72,11 @@ const SportFriend = () => {
   return (
     <View>
       <Text>SportFriend</Text>
+      <FlatList
+            data={friends}
+            renderItem={renderFriendsNames}
+            keyExtractor={(item) => item}
+        />
     </View>
   )
 }
