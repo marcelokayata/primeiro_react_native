e0eaa7bb2aa02f713c4df425bcbc4a97d2f6721a
Marcelo Apovian Kayata <marcelo_kayata@hotmail.com>
2024-12-22 12:42:24 -0300

    add picture

--- src/Screens/SportFriend.js
+++ src/Screens/SportFriend.js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, StyleSheet  } from 'react-native'
+import { Image, View, Text, FlatList, StyleSheet  } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { collection, getDocs, query, where,  doc,  updateDoc, addDoc } from 'firebase/firestore';
 import { userRef, pageUsersRef, db } from '../../firebase/config';
@@ -14,25 +14,34 @@ const SportFriend = () => {
   const renderFriendsNames = ({ item }) => (
     <Text>{item}</Text>
   );
+  const userAvatar = require("../../assets/man.png")
   useEffect(()=>  {
     const fetchData = async () => {
+      try{
       const querySnapshot = await getDocs(collection(db, 'Users'));
       const userData = querySnapshot.docs.map(doc => ({
         ...doc.data(),
         id: doc.id
       }));
       setUsersData(userData);
-      
+      console.log("userData aqui: ", userData)
+      }
+      catch (error) {
+        console.error('Error fetching data:', error);
+      }
 
     };
     fetchData()
-    console.log("Dados aqui: ", usersData)
+    console.log("usersData aqui: ", usersData)
   }, [])
   return (
     <FlatList
       data={usersData}
       renderItem={({ item }) => (
         <View>
+          <Image source={userAvatar}  className="h-12 w-12 mx-3"/>
+          
+          <Image source={{uri:item.profilePic}} className="h-12 w-12 rounded-full mx-3" />
           {/* Render your array data here */}
           <Text>{item.username}</Text>
           {/* Access other array fields as needed */}
